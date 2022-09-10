948182a6a3cc9daa805cb05f965d69f27458e36d
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-09-10 23:20:38 +0900

    add styled components

--- client/src/pages/auth/Auth.tsx
+++ client/src/pages/auth/Auth.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { render } from "react-dom";
+import styled from "styled-components";
+
+const AuthContainer = styled.div`
+  position: absolute;
+
+  width: 80%;
+  height: 80vh;
+
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  border: 1px solid black;
+`;
 
 const Auth = () => {
-  return <h1>Auth</h1>;
-};
+  const [loginOrSignin, setLoginOrSignin] = useState("Log In");
 
+  const handleLoginOrSignin = () => {
+    setLoginOrSignin((prev) => (prev === "Log In" ? "Sign In" : "Log In"));
+  };
+
+  useEffect(() => {}, []);
+  return (
+    <AuthContainer>
+      <h1>{loginOrSignin}</h1>
+      {/* <AuthForm loginOrSignin={loginOrSignin}></AuthForm> */}
+      <p onClick={handleLoginOrSignin}>
+        {loginOrSignin === "Log In" ? "Sign In" : "Log In"}
+      </p>
+    </AuthContainer>
+  );
+};
 export default Auth;
