ffc83f7841e064566f49f0b6423d65ebff3cdc45
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-09-11 22:14:34 +0900

    [divide] CreateTodo

--- client/src/components/todo/CreateTodo.tsx
+++ client/src/components/todo/CreateTodo.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import useInput from "../../hooks/useInput";
+import { fetchCreateTodo } from "../../services/api/todoAPI";
+interface Props {
+  onCreateNewTodo: () => void;
+}
 
-const CreateTodo = () => {
-  return;
+const CreateTodo = ({ onCreateNewTodo }: Props) => {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const contentRef = useRef<HTMLInputElement>(null);
+
+  const inputTitle = useInput("");
+  const inputContent = useInput("");
+
+  useEffect(() => {
+    titleRef.current?.focus();
+  }, []);
+
+  const isValidInput = () => {
+    if (!inputTitle.value) {
+      titleRef.current?.focus();
+      return false;
+    } else if (!inputContent.value) {
+      contentRef.current?.focus();
+      return false;
+    }
+    return true;
+  };
+
+  const onSubmitNewTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isValidInput()) {
+      fetchCreateTodo({ title: inputTitle.value, content: inputContent.value });
+      onCreateNewTodo();
+    }
+  };
+
+  return (
+    <div>
+      <h1>새로운 TODO 만들기</h1>
+      <form action="submit" onSubmit={onSubmitNewTodo}>
+        <label htmlFor="title">TITLE : </label>
+        <input
+          type="text"
+          id="title"
+          ref={titleRef}
+          {...inputTitle}
+          placeholder="TODO의 제목을 작성해주세요"
+        />
+        <label htmlFor="content">CONTENT : </label>
+        <input
+          type="text"
+          id="content"
+          ref={contentRef}
+          {...inputContent}
+          placeholder="TODO의 설명을 작성해주세요"
+        />
+        <button>TODO 추가 👻</button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateTodo;
