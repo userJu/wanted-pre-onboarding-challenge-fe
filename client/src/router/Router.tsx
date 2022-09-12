e1d18638b1095b45af627acc106b818aa95fdc54
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-09-12 10:59:23 +0900

    [router] ToDo 경로 추가

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Auth from "../pages/auth/Auth";
-import { AUTH } from "./routerPath";
+import Todo from "../pages/todo/Todo";
+import { AUTH, TODO } from "./routerPath";
 
 const Router = () => {
   return (
     <Routes>
       <Route path={AUTH} element={<Auth />} />
+      <Route path={TODO} element={<Todo />} />
     </Routes>
   );
 };
